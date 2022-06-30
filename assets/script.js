74f3e66c35751d94898776250d3bd5d2683cfcb5
Thorgathis <nicolas.kopytin@gmail.com>
2022-06-30 14:32:00 +0300

    ver. 1.3

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+function ThemeSet() {
+    if (localStorage.getItem("theme") === "light") {
+        $("body").removeClass("backg-dark");
+        $("nav")
+            .removeClass("navbar-dark bg-dark")
+            .addClass("navbar-light bg-light");
+        $(".text").removeClass("col-gray-white");
+        $(".pbar").removeClass("pbar-bg");
+        $(".card").removeClass("card-dark");
+    } else {
+        $("body").addClass("backg-dark");
+        $("nav")
+            .addClass("navbar-dark bg-dark")
+            .removeClass("navbar-light bg-light");
+        $(".text").addClass("col-gray-white");
+        $(".pbar").addClass("pbar-bg");
+        $(".card").addClass("card-dark");
+    }
+}
+var today = new Date().getHours();
+console.log(today);
+if ((today <= 6 && today >= 0) || (today <= 23 && today >= 14)) {
+    localStorage.setItem("theme", "dark");
+} else {
+    localStorage.setItem("theme", "light");
+}
+ThemeSet();
+
+let buttonPlay = document.querySelector("button");
+buttonPlay.onclick = function () {
+    this.parentElement.innerHTML = "<div>Тут будет iframe</div>";
+};
