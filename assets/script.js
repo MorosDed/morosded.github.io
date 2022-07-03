2c1fad516aca88a84a89883e3853164974ec326a
Thorgathis <nicolas.kopytin@gmail.com>
2022-07-03 12:17:14 +0300

    theme toggie

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,21 @@
+function ThemeToggle() {
+    if (localStorage.getItem("theme") === "dark") {
+        localStorage.setItem("theme", "light");
+    } else {
+        localStorage.setItem("theme", "dark");
+    }
+    ThemeSet();
+}
+
 function ThemeSet() {
     if (localStorage.getItem("theme") === "light") {
-        $("body").removeClass("backg-dark");
+        $("body").removeClass("dark-bg").addClass("light-bg");
         $("nav")
             .removeClass("navbar-dark bg-dark")
             .addClass("navbar-light bg-light");
         $(".text").removeClass("col-gray-white");
     } else {
-        $("body").addClass("backg-dark");
+        $("body").addClass("dark-bg").removeClass("light-bg");
         $("nav")
             .addClass("navbar-dark bg-dark")
             .removeClass("navbar-light bg-light");
@@ -15,7 +24,7 @@ function ThemeSet() {
 }
 var today = new Date().getHours();
 console.log(today);
-if ((today <= 6 && today >= 0) || (today <= 23 && today >= 14)) {
+if ((today <= 6 && today >= 0) || (today <= 23 && today >= 20)) {
     localStorage.setItem("theme", "dark");
 } else {
     localStorage.setItem("theme", "light");
